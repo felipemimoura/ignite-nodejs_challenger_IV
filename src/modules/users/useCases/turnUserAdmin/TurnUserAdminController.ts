40b0a8a0b171c71598946e3eebe2c54cf3643dc1
Felipe Moura <felipemimoura@gmail.com>
2022-08-04 18:41:27 -0300

    feat: finishid all test

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,14 +3,18 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
-    const { user_id } = request.body;
-    const user = this.turnUserAdminUseCase.execute(user_id);
+    const { user_id } = request.params;
 
-    return response.json(user);
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
